c8b3f6c258be6e7e4c6f702014fabc26834837ae
panpan2001 <truonghoc19102001@gmail.com>
2025-11-15 17:17:06 +0700

    first commit - create project

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from helpers.logger import other_common_logger
 # from utils import test_db_file_path
 # from helpers import connect_db, close_db, excel_to_sql, csv_to_sql, read_db
 
-# from functions import check_config
+from functions import check_config
 
-# check_config("configs/data_source.xlsx")
+check_config("configs/data_source.xlsx")
 
 # Check file data_source_config.xlsx is exist or not, if not, log error and end program.
-from helpers import check_file_exist
-check_file_exist("configs/test/data_source.xlsx")
+# from helpers import check_file_exist
+# check_file_exist("configs/test/data_source.xlsx")
 
 # Connect to database
 
