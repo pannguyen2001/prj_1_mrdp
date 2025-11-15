6ebac7fdd8529c3f5b4979df4abc00e058d6ea97
anotherone2xxx <anotherone2xxx@gmail.com>
2025-11-15 10:53:54 +0000

    pannguyen - first commit

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from helpers.logger import other_common_logger
 # check_config("configs/data_source.xlsx")
 
 # Check file data_source_config.xlsx is exist or not, if not, log error and end program.
-from helpers import check_file_exist
-check_file_exist("configs/test/data_source.xlsx")
+# from helpers import check_file_exist
+# check_file_exist("configs/test/data_source.xlsx")
 
 # Connect to database
 
